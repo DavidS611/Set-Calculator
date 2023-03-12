05485349884b89628f827809fd80a31fe0732869
DavidS611 <davidx558@gmail.com>
2023-03-12 19:12:10 +0200

    Update 1.1

--- git.sh
+++ git.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Define Git function
+function git() {
+    /usr/bin/git "$@"
+}
+
 # Git target
-git:
-	git add "$1"
-	# optional body comment
-	if [[ -n "$3" ]]; then
-		git commit -m "$2" -m "$3"
-	else
-		git commit -m "$2"
-	fi
-	git push -u origin master
+git add "$1"
+# optional body comment
+if [[ -n "$3" ]]; then
+    git commit -m "$2" -m "$3"
+else
+    git commit -m "$2"
+fi
+git push -u origin master
 
