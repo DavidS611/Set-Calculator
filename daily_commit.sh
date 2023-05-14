4a602ead6e286686a6dffacd1f8a9099f6085ec6
DavidS611 <davidx558@gmail.com>
2023-05-14 23:36:58 +0300

    Daily commit

--- daily_commit.sh
+++ daily_commit.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Set the path to the file that contains the access token
+TOKEN_FILE=/home/david/Documents/Degree/Projects/token.txt
+
+# Read the access token from the file
+GITHUB_TOKEN=$(cat "$TOKEN_FILE")
+
+# Add and commit changes
 git add .
 git commit -m "Daily commit"
-git push 
+
+# Push changes with authentication using the access token
+git push https://$GITHUB_TOKEN@github.com/<DavidS611/Set-Calculator.git
 
